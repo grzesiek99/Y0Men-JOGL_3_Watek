7fc550dfb2e8534e8d43190feea7ccd7f0b800bc
student <student@240-03.INFORMATYKA.local>
2016-10-06 13:24:18 +0200

    Zadanie 2 - rysowanie czworotkata

--- src/org/yourorghere/GWatek.java
+++ src/org/yourorghere/GWatek.java
@@ -95,6 +95,12 @@ gl.glVertex3f(-1.0f, 1.0f, -6.0f);
 gl.glVertex3f(-2.0f,-1.0f, -6.0f);
 gl.glVertex3f( 0.0f,-1.0f, -6.0f);
 gl.glEnd();
+gl.glBegin(GL.GL_QUADS);
+gl.glVertex3f(1.0f, 1.0f, -7.0f);
+gl.glVertex3f(2.0f,1.0f, -7.0f);
+gl.glVertex3f(2.0f,-1.0f, -7.0f);
+gl.glVertex3f(1.0f,-1.0f, -7.0f);
+gl.glEnd();
 
 
  //Wykonanie wszystkich operacji znajduj¹cych siê w buforze
