eb75aa2be3e14408df81df3fc62ff971a6653a9f
student <student@240-10.INFORMATYKA.local>
2016-12-01 14:53:28 +0200

    ZAD1 szescian z texturami

--- src/org/yourorghere/GWalat.java
+++ src/org/yourorghere/GWalat.java
@@ -12,6 +12,13 @@ import javax.media.opengl.GLAutoDrawable;
 import javax.media.opengl.GLCanvas;
 import javax.media.opengl.GLEventListener;
 import javax.media.opengl.glu.GLU;
+import com.sun.opengl.util.texture.Texture;
+import com.sun.opengl.util.texture.TextureIO;
+import java.io.File;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import javax.swing.JOptionPane;
+
 
 
 
@@ -52,7 +59,7 @@ public class GWalat implements GLEventListener {
             }
         });
         
-        //OBs³uga klawiszy strza³ek
+        //OBs?uga klawiszy strza?ek
         frame.addKeyListener(new KeyListener()
         {
             public void keyPressed(KeyEvent e)
@@ -75,23 +82,64 @@ public class GWalat implements GLEventListener {
         frame.setVisible(true);
         animator.start();
     }
+    
+    static BufferedImage image1 = null,image2 = null;
+    static Texture t1 = null, t2 = null;
+
 
     public void init(GLAutoDrawable drawable) {
         // Use debug pipeline
         // drawable.setGL(new DebugGL(drawable.getGL()));
 
         GL gl = drawable.getGL();
-        System.err.println("INIT GL IS: " + gl.getClass().getName());
+ System.err.println("INIT GL IS: " + gl.getClass().getName());
+ gl.setSwapInterval(1);
 
-        // Enable VSync
-        gl.setSwapInterval(1);
 
-        // Setup the drawing area and shading mode
-        gl.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-        gl.glShadeModel(GL.GL_SMOOTH); // try setting this to GL_FLAT and see what happens.
-        
-        //wy³¹czenie wewnetrznych stron prymitywów
-        gl.glEnable(GL.GL_CULL_FACE);
+ //wartoœci sk³adowe oœwietlenia i koordynaty Ÿród³a œwiat³a
+ float ambientLight[] = { 0.3f, 0.3f, 0.3f, 1.0f };//swiat³o otaczaj¹ce
+ float diffuseLight[] = { 0.7f, 0.7f, 0.7f, 1.0f };//œwiat³o rozproszone
+ float specular[] = { 1.0f, 1.0f, 1.0f, 1.0f}; //œwiat³o odbite
+ float lightPos[] = { 0.0f, 150.0f, 150.0f, 1.0f };//pozycja œwiat³a
+ //(czwarty parametr okreœla odleg³oœæ Ÿród³a:
+ //0.0f-nieskoñczona; 1.0f-okreœlona przez pozosta³e parametry)
+ gl.glEnable(GL.GL_LIGHTING); //uaktywnienie oœwietlenia
+ //ustawienie parametrów Ÿród³a œwiat³a nr. 0
+ gl.glLightfv(GL.GL_LIGHT0,GL.GL_AMBIENT,ambientLight,0); //swiat³o otaczaj¹ce
+ gl.glLightfv(GL.GL_LIGHT0,GL.GL_DIFFUSE,diffuseLight,0); //œwiat³o rozproszone
+ gl.glLightfv(GL.GL_LIGHT0,GL.GL_SPECULAR,specular,0); //œwiat³o odbite
+ gl.glLightfv(GL.GL_LIGHT0,GL.GL_POSITION,lightPos,0); //pozycja œwiat³a
+ gl.glEnable(GL.GL_LIGHT0); //uaktywnienie Ÿród³a œwiat³a nr. 0
+ gl.glEnable(GL.GL_COLOR_MATERIAL); //uaktywnienie œledzenia kolorów
+ //kolory bêd¹ ustalane za pomoc¹ glColor
+ gl.glColorMaterial(GL.GL_FRONT, GL.GL_AMBIENT_AND_DIFFUSE);
+ gl.glEnable(GL.GL_DEPTH_TEST);
+ // Setup the drawing area and shading mode
+ gl.glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+ gl.glShadeModel(GL.GL_SMOOTH);
+ try
+ {
+ image1 = ImageIO.read(getClass().getResourceAsStream("/pokemon.jpg"));
+ image2 = ImageIO.read(getClass().getResourceAsStream("/android.jpg"));
+ }
+ catch(Exception exc)
+ {
+ JOptionPane.showMessageDialog(null, exc.toString());
+ return;
+ }
+
+ t1 = TextureIO.newTexture(image1, false);
+ t2 = TextureIO.newTexture(image2, false);
+
+ gl.glTexEnvi(GL.GL_TEXTURE_ENV, GL.GL_TEXTURE_ENV_MODE, GL.GL_BLEND | GL.GL_MODULATE);
+ gl.glEnable(GL.GL_TEXTURE_2D);
+ gl.glTexParameteri(GL.GL_TEXTURE_2D, GL.GL_TEXTURE_WRAP_S, GL.GL_REPEAT);
+ gl.glTexParameteri(GL.GL_TEXTURE_2D, GL.GL_TEXTURE_WRAP_T, GL.GL_REPEAT);
+ gl.glTexParameteri(GL.GL_TEXTURE_2D, GL.GL_TEXTURE_MAG_FILTER, GL.GL_NEAREST);
+ gl.glTexParameteri(GL.GL_TEXTURE_2D, GL.GL_TEXTURE_MIN_FILTER, GL.GL_NEAREST);
+ gl.glBindTexture(GL.GL_TEXTURE_2D, t1.getTextureObject());
+ 
+
     }
 
     public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
@@ -114,7 +162,7 @@ public class GWalat implements GLEventListener {
     public void createShape(GL gl, float px, float py, float pz , float size) {
         float kat, x, y;
         gl.glBegin(GL.GL_TRIANGLE_FAN);
-        gl.glVertex3f(px,py,pz); //œrodek
+        gl.glVertex3f(px,py,pz); //?rodek
         for(kat = 0.0f; kat < (2.0f*Math.PI);
         kat+=(Math.PI/32.0f))
         {
@@ -170,34 +218,55 @@ GL gl = drawable.getGL();
  gl.glTranslatef(0.0f, 0.0f, -6.0f);   //przesuniecie o 6 jednostek
  gl.glRotatef(xrot, 1.0f, 0.0f, 0.0f); //rotacja wokol osi X
  gl.glRotatef(yrot, 0.0f, 1.0f, 0.0f); //rotacja wokol osi Y
-        sierpin(gl,1f,1f,-1f,-1f,2f,2f,1);
- //Wykonanie wszystkich operacji znajduj¹cych siê w buforze
-
-
-float xx,yy,katt;
-gl.glBegin(GL.GL_TRIANGLE_FAN);
+       // sierpin(gl,1f,1f,-1f,-1f,2f,2f,1);
+ //Wykonanie wszystkich operacji znajduj?cych si? w buforze
+ gl.glBindTexture(GL.GL_TEXTURE_2D, t1.getTextureObject());
+ gl.glBegin(GL.GL_QUADS);
+//sciana gorna
+gl.glColor3f(1.0f,0.0f,0.0f);
+gl.glNormal3f(0.0f, 0.0f, 1.0f);
+gl.glTexCoord2f(1.0f, 0.0f); gl.glVertex3f(1.0f,1.0f,-1.0f);
+gl.glTexCoord2f(0.0f, 0.0f); gl.glVertex3f(-1.0f,1.0f,-1.0f);
+gl.glTexCoord2f(0.0f, 1.0f); gl.glVertex3f(-1.0f,1.0f,1.0f);
+gl.glTexCoord2f(1.0f, 1.0f); gl.glVertex3f(1.0f,1.0f,1.0f); 
+//?ciana przednia
+gl.glColor3f(1.0f,1.0f,0.0f);
+gl.glNormal3f(0.0f, 0.0f, 1.0f);
+gl.glTexCoord2f(1.0f, 1.0f); gl.glVertex3f(-1.0f,-1.0f,1.0f);
+gl.glTexCoord2f(0.0f, 1.0f); gl.glVertex3f(1.0f,-1.0f,1.0f);
+gl.glTexCoord2f(0.0f, 0.0f); gl.glVertex3f(1.0f,1.0f,1.0f);
+gl.glTexCoord2f(1.0f, 0.0f); gl.glVertex3f(-1.0f,1.0f,1.0f);
+//sciana tylnia
 gl.glColor3f(0.0f,1.0f,0.0f);
-gl.glVertex3f(0.0f,1.0f,0.0f); //?rodek
-for(katt = 0.0f; katt < (2.0f*Math.PI);
-katt+=(Math.PI/32.0f))
-{
-xx = 1.0f*(float)Math.sin(katt);
-yy = 1.0f*(float)Math.cos(katt);
-gl.glVertex3f(xx, -1.0f, yy); //kolejne punkty
-}
+gl.glNormal3f(1.0f, 0.0f, 1.0f);
+gl.glTexCoord2f(0.0f, 0.0f); gl.glVertex3f(-1.0f,1.0f,-1.0f);
+gl.glTexCoord2f(1.0f, 0.0f); gl.glVertex3f(1.0f,1.0f,-1.0f);
+gl.glTexCoord2f(1.0f, 1.0f); gl.glVertex3f(1.0f,-1.0f,-1.0f);
+gl.glTexCoord2f(0.0f, 1.0f); gl.glVertex3f(-1.0f,-1.0f,-1.0f);
 gl.glEnd();
+gl.glBindTexture(GL.GL_TEXTURE_2D, t2.getTextureObject());
+gl.glBegin(GL.GL_QUADS);
 
-float xxx,yyy,kattt;
-gl.glBegin(GL.GL_TRIANGLE_FAN);
-gl.glColor3f(1.0f,1.0f,1.0f);
-gl.glVertex3f(0.0f,-1.0f,0.0f); //?rodek
-for(kattt = (float) (2.0f*Math.PI); kattt > 0.0f;
-kattt-=(Math.PI/32.0f))
-{
-xxx = 1.0f*(float)Math.sin(kattt);
-yyy = 1.0f*(float)Math.cos(kattt);
-gl.glVertex3f(xxx, -1.0f, yyy); //kolejne punkty
-}
+//?ciana lewa
+gl.glColor3f(0.0f,0.0f,1.0f);
+gl.glNormal3f(0.0f, 0.0f, 1.0f);
+gl.glTexCoord2f(0.0f, 0.0f); gl.glVertex3f(-1.0f,-1.0f,-1.0f);
+gl.glTexCoord2f(1.0f, 0.0f); gl.glVertex3f(-1.0f,-1.0f,1.0f);
+gl.glTexCoord2f(1.0f, 1.0f); gl.glVertex3f(-1.0f,1.0f,1.0f);
+gl.glTexCoord2f(0.0f, 1.0f); gl.glVertex3f(-1.0f,1.0f,-1.0f);
+//?ciana prawa
+gl.glColor3f(1.0f,1.0f,0.0f);
+gl.glNormal3f(0.0f, 0.0f, 1.0f);
+gl.glTexCoord2f(0.0f, 1.0f); gl.glVertex3f(1.0f,1.0f,-1.0f);
+gl.glTexCoord2f(0.0f, 0.0f); gl.glVertex3f(1.0f,1.0f,1.0f);
+gl.glTexCoord2f(1.0f, 0.0f); gl.glVertex3f(1.0f,-1.0f,1.0f);
+gl.glTexCoord2f(0.0f, 0.0f); gl.glVertex3f(1.0f,-1.0f,-1.0f);
+//?ciana dolna
+gl.glColor3f(1.0f,0.0f,1.0f);
+gl.glTexCoord2f(0.0f, 1.0f); gl.glVertex3f(-1.0f,-1.0f,1.0f);
+gl.glTexCoord2f(1.0f, 1.0f); gl.glVertex3f(-1.0f,-1.0f,-1.0f);
+gl.glTexCoord2f(1.0f, 0.0f); gl.glVertex3f(1.0f,-1.0f,-1.0f);
+gl.glTexCoord2f(0.0f, 0.0f); gl.glVertex3f(1.0f,-1.0f,1.0f);
 gl.glEnd();
  
  gl.glFlush();
